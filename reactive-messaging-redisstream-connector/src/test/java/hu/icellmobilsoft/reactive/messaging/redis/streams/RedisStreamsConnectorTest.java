827414252b51eef6499d6fff0e79dae11a6e52b3
Péter Szabó <peter.szabo@icellmobilsoft.hu>
2025-03-12 13:55:18 +0100

    #1 - license in test

--- reactive-messaging-redisstream-connector/src/test/java/hu/icellmobilsoft/reactive/messaging/redis/streams/RedisStreamsConnectorTest.java
+++ reactive-messaging-redisstream-connector/src/test/java/hu/icellmobilsoft/reactive/messaging/redis/streams/RedisStreamsConnectorTest.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * reactive-redisstream-messaging
+ * %%
+ * Copyright (C) 2025 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package hu.icellmobilsoft.reactive.messaging.redis.streams;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +28,8 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import jakarta.inject.Inject;
 
 import org.jboss.logging.JBossLogManagerProvider;
 import org.jboss.weld.junit5.EnableWeld;
@@ -39,8 +60,6 @@ import io.lettuce.core.RedisURI;
 import io.lettuce.core.StreamMessage;
 import io.lettuce.core.XReadArgs;
 import io.lettuce.core.models.stream.PendingMessages;
-
-import jakarta.inject.Inject;
 
 /**
  * Test class for {@link RedisStreamsConnector}
