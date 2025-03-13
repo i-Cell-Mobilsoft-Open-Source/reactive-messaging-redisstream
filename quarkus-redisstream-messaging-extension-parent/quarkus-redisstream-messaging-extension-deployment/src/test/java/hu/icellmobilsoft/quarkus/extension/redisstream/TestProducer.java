7578d4985253b6b7b30bd33be11c0d0eb20e7035
Martin Nagy <martin.nagy@icellmobilsoft.hu>
2025-03-13 10:21:10 +0100

    #8 Improve concurrency

--- quarkus-redisstream-messaging-extension-parent/quarkus-redisstream-messaging-extension-deployment/src/test/java/hu/icellmobilsoft/quarkus/extension/redisstream/TestProducer.java
+++ quarkus-redisstream-messaging-extension-parent/quarkus-redisstream-messaging-extension-deployment/src/test/java/hu/icellmobilsoft/quarkus/extension/redisstream/TestProducer.java
@@ -25,7 +25,7 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
-import hu.icellmobilsoft.reactive.messaging.redis.streams.RedisStreamMetadata;
+import hu.icellmobilsoft.reactive.messaging.redis.streams.metadata.RedisStreamMetadata;
 import io.smallrye.reactive.messaging.providers.locals.ContextAwareMessage;
 
 /**
